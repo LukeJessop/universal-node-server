d31c857550d92e7e58298aae78051d3019ad72b2
LukeJessop <jessop.lucas@gmail.com>
2023-11-06 11:42:22 -0700

    fixed endpoint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-app.get('/ping', () => 'pong')
+app.get('/ping', (req, res) => res.send('Pong'))
 
 const port = 4040
 app.listen(port, console.log('Server up on port: ' + port))
